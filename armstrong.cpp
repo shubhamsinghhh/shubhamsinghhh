f8aa31fea9406070b545f38279d4b2ba72845e89
shubhamsinghhh <91362009+shubhamsinghhh@users.noreply.github.com>
2021-09-29 10:05:02 +0530

    Add files via upload

---
+++ armstrong.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+  int n ,r,sum=0,temp;
+  cout<<" enter the number";
+cin>>n;
+temp>>n;
+ while(n>0)
+{
+r=n%10;
+sum=sum+(r*r*r); 
+n=n/10;
+}
+if (temp==sum)
+cout<<"armstrong number,"<<endl;
+else
+cout<<" Not armstrong number."<<endl;
+return 0;
+}
+
+
+
